36e96767a5a22981c1392c67c54564d60f94e373
g1ann0z <giannozcydia@gmail.com>
2023-12-09 21:07:21 +0100

    lentezza-ng-serve-fixato

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    server: {
+      fs: {
+        allow: ['/Users/giannoz/Downloads/repository_michele_fenu/tnv-academy-XI/the-net-pizza-final/pizze/giovanni-deiana/giovanni-deiana-angular'],
+      },
+    },
+  };
