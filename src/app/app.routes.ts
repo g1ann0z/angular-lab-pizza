7f58c86b7db947a4dfccaf365cbc31ee9fbe8d83
g1ann0z <giannozcydia@gmail.com>
2023-12-09 17:07:51 +0100

    lesson02_0h12m

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PiattoDetailComponent } from './components/piatto-detail/piatto-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'prova-menu-detail', component: PiattoDetailComponent}
+];
---
+++ src/app/components/piatto-detail/piatto-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gnnz-piatto-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './piatto-detail.component.html',
+  styleUrl: './piatto-detail.component.scss'
+})
+export class PiattoDetailComponent {
+
+}
